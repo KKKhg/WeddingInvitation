64412f8a0d46b450a0991068c62af033ac3476a2
KKKhg <rkdrn0932@>
2023-01-31 20:46:15 +0900

    [Test] Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import zippo from './assets/images/zippo.jpeg'
 
 function App() {
   return (
     <div>
         자기 언능 와용
-        <img src={zippo} width="100%" alt="zippo"/>
+        <img src={require('./assets/images/zippo.jpeg')} width="100%" alt="zippo"/>
+        <img src={'http://49.50.164.104:8000/pic/zippo.jpeg'} width="100%" alt="zippo"/>
     </div> 
   );
 }
