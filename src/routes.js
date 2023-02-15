eaa929c546617337151cc01eea208e50949ac510
KKKhg <>
2023-02-16 01:35:53 +0900

    [Fix] BrowserRouter => HashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Sample from "./pages/Sample";
 
