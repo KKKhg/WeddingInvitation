b65e71f29be47fc114a237bc73b740cb794d20ff
KKKhg <lionskang@gmail.com>
2023-02-19 04:54:26 +0900

    [FIX] Video Test

--- src/component/VideoPlayer.js
+++ src/component/VideoPlayer.js
@@ -5,7 +5,7 @@ import './VideoPlayer.css';
 const VideoPlayer = () => {
     return (
         <div className="video_wrapper">
-            <video className="video"  
+            {/* <video className="video"  
                 autoPlay={true}
                 muted={true}
                 controls={false}
@@ -13,15 +13,15 @@ const VideoPlayer = () => {
                 playsInline={true}
             >
                 <source src={`${process.env.PUBLIC_URL}/videos/weddingVideo.mp4`} type="video/mp4" />
-            </video>
+            </video> */}
             
             <ReactPlayer 
                     url={`${process.env.PUBLIC_URL}/videos/weddingVideo.mp4`}
+                    autoPlay={true}
                     playing={true}
                     playsinline={true}
-                    pre
                     loop={true}
-                    volume={0}
+                    muted={true}
                     width='100%'
                     height='100%'
             />  
