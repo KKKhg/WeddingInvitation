0223869464be933af74435dc6e1aa0356fdecb48
KKKhg <lionskang@gmail.com>
2023-02-26 01:58:24 +0900

    [ADD] 길찾기앱 연동 추가

--- src/component/adress/Address.js
+++ src/component/adress/Address.js
@@ -1,18 +1,59 @@
 import './Address.css';
-import KakaoMap from "./KakaoMap";
+import Map from "./Map";
 
 const Address = () => {
   
     return (
-        <div className="adress_wrap">
+        <div className="address_wrap">
             <div>오시는 길</div>
-            <KakaoMap />
-            <div className="adress_text_wrap">
-                <table>
+            <Map />
+            <div className="address_text_wrap">
+                <table className="address_table">
                     <tbody>
                         <tr>
-                            <th>도로명 주소: </th>
+                            <th>도로명 주소:</th>
                             <td>서울특별시 강남구 언주로 508</td>
+                        </tr>
+                        <tr>
+                            <th>지번 주소:</th>
+                            <td>서울특별시 강남구 역삼동 701번지</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr className="address_table_subject">
+                            <th colSpan={2}>🚌 교통편</th>
+                        </tr>
+                        <tr>
+                            <th>지하철</th>
+                            <td>
+                                2호선, 분당선 "선릉역" 5번출구 도보 5분<br/>
+                                선릉역 5번출구에서 셔틀버스 운행 (5분간격)
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>버스</th>
+                            <td>
+                                KT 강남지사 하차<br/>
+                                <span className="blue">간선버스:</span> 141 (도봉산 ↔ 염곡동)<br/>
+                                <span className="green">지선버스:</span> 3422 (장지동 ↔ 압구정동)<br/><br/>
+                                한국기술센터, 상록회관 하차<br/>
+                                <span className="blue">간선버스:</span> 146 (상계동 ↔ 강남역)<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                146 (상계동 ↔ 강남역)<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                350 (송파 ↔ 여의도)<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                740 (덕은동 ↔ 잠실역)<br/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>자가용</th>
+                            <td>
+                                네비게이션 "서울상록회관" 또는 "서울시 강남구 언주로 508" 입력<br/>
+                                동시주차 500대 가능
+                            </td>
                         </tr>
                     </tbody>
                 </table>
---
+++ src/component/adress/Map.js
@@ -0,0 +1,88 @@
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import './Map.css';
+import isMobile from '../../utils/isMobile';
+import { SiKakao } from 'react-icons/si';
+
+const Map = () => {
+    const [cnt, setCnt] = useState(0);
+    const lat = 37.503874;
+    const lon = 127.042873
+    
+    useEffect(() => {
+        if(cnt < 1) setCnt(prev => ++prev);
+        else if(cnt == 1) {
+ 
+            const container = document.querySelector('.map');
+            const options = {
+                center: new window.kakao.maps.LatLng(lat, lon),
+                level: 3
+            }
+            const map = new window.kakao.maps.Map(container, options);
+            
+            // 마커 표시
+            const markerPosition  = new window.kakao.maps.LatLng(lat, lon); 
+            const marker = new window.kakao.maps.Marker({
+                position: markerPosition
+            });
+            marker.setMap(map);
+            
+            // 줌 컨트롤러 생성
+            const zoomControl = new window.kakao.maps.ZoomControl();
+            map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+        }
+    }, [cnt]);
+    
+    function kakaoMapLinkTo() {
+        window.open('https://map.kakao.com/link/to/2009675378', '_blank');
+    }
+    
+    function tMapLinkTo() {
+        if(!isMobile()) return alert('모바일에서만 사용 가능합니다.');
+        window.open(`https://apis.openapi.sk.com/tmap/app/routes?appKey=AvH2FYuxSc6ARNU1ejGGj8UYBwiZavbQ3Gib84QS&name=상록아트홀&lon=${lon}&lat=${lat}`, '_blank');
+        
+    }
+    
+    function naverMapLinkTo() {
+        if(!isMobile()) return alert('모바일에서만 사용 가능합니다.');
+        let url = `nmap://route/public?parameter=value&appname=WeddingInvitation`;
+        url += `&dlat=${lat}`;
+        url += `&dlng=${lon}`;
+        url += `&dname=${encodeURI('상록아트홀')}`
+        window.open(url, '_blank');
+    }
+    
+    
+    
+    return (
+        <div className="map_wrapper">
+            <div>
+                <div className="map">
+                </div>
+            </div>
+            <div className="map_btn_wrapper">
+                
+            </div>
+            <Button variant="contained"
+                startIcon={<img className="map_icon" src={require('../../assets/images/kakao_map.png')}/>}
+                onClick={kakaoMapLinkTo}
+            >
+                길찾기
+            </Button>
+            <Button variant="contained"
+                startIcon={<img className="map_icon" src={require('../../assets/images/naver_map.png')}/>}
+                onClick={naverMapLinkTo}
+            >
+                길찾기
+            </Button>
+            <Button variant="contained"
+                startIcon={<img className="map_icon" src={require('../../assets/images/t_map.png')}/>}
+                onClick={tMapLinkTo}
+            >
+                길찾기
+            </Button>
+        </div>
+    );
+};
+
+export default Map;
