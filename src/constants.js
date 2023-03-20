2eebbb5c075956a93e28d4244fae08c9a994a76b
KKKhg <lionskang@gmail.com>
2023-03-20 21:59:27 +0900

    [FIX}

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// export const BASEURL = `${process.env.REACT_APP_BASEURL}`
-export const BASEURL = `http://192.168.0.103:8001/`
+export const BASEURL = `${process.env.REACT_APP_BASEURL}`
+// export const BASEURL = `http://192.168.0.103:8001/`
 // export const BASEURL = `https://kkkhg.site/`
 export const APIPATH = {
     WEDDING: 'wedding/'
